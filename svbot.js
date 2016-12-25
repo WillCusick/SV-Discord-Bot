e13cc89b5ed6f4ce5def960bfb5e92869794b155
ElDynamite <eldynamiter@gmail.com>
2016-12-25 17:25:48 -0500

    check guilds

--- svbot.js
+++ svbot.js
@@ -48,6 +48,8 @@ bot.on("message", msg => {
                 cleanChannel(msg, msg.channel);
             } else if (command == "help") {
                 helpCommand(msg);
+            } else if (command == "howmanyguilds") {
+                checkGuilds(msg);
             } else {
                 cardSearchCommand(["card-search"].concat(args), msg);
             }
@@ -79,6 +81,15 @@ bot.on("disconnected", () => {
 });
 
 //MESSAGE HANDLING
+
+function checkGuilds(msg) {
+    let gArray = bot.guilds.array();
+    let totalUsers = 0;
+    for (var ii=0;ii<gArray.length;ii++) {
+        totalUsers += gArray[ii].memberCount;
+    }
+    sendMessage(msg.channel, `Logged onto ${gArray.length} servers with a total of ${totalUsers} members`);
+}
 
 function sendMessage(channel, message) {
     channel.sendMessage(message)
